988c83bce2ca68493e0f8a7657c9f4ac0aa1fdfa
phueb <info@philhuebner.com>
2019-11-13 12:16:31 -0600

    remove unused functions

--- wordplay/location.py
+++ wordplay/location.py
@@ -11,26 +11,11 @@ def make_w2locations(tokens):
     return result
 
 
-def probes_reordered_loc():
-    n_sum = 0
-    num_ns = 0
-    for n, term in enumerate(self.reordered_tokens):
-        if term in self.probe_store.types:
-            n_sum += n
-            num_ns += 1
-    result = n_sum / num_ns
-    return result
-
-
-def split_probes_by_loc(num_splits, is_reordered=False):
-    if is_reordered:
-        d = self.term_avg_reordered_loc_dict
-    else:
-        d = self.term_avg_unordered_loc_dict
-    probe_loc_pairs = [(probe, loc) for probe, loc in d.items()
-                       if probe in self.probe_store.types]
+def split_probes_by_loc(w2avg_location, probe_store, num_splits):
+    probe_loc_pairs = [(w, loc) for w, loc in w2avg_location.items()
+                       if w in probe_store.types]
     sorted_probe_loc_pairs = sorted(probe_loc_pairs, key=lambda i: i[1])
-    num_in_split = self.probe_store.num_probes // num_splits
+    num_in_split = probe_store.num_probes // num_splits
     for split_id in range(num_splits):
         start = num_in_split * split_id
         probes, _ = zip(*sorted_probe_loc_pairs[start: start + num_in_split])
@@ -49,11 +34,4 @@ def make_locations_xy(w2locations: Dict[str, List[int]],
     locations_list = list(chain(*word_locations_list))
     hist, b = np.histogram(locations_list, bins=num_bins)
     result = (b[:-1], np.squeeze(hist))
-    return result
-
-
-def calc_loc_asymmetry(term, num_bins=200):  # num_bins=200 for terms
-    (x, y) = self.make_locs_xy([term], num_bins=num_bins)
-    y_fitted = self.fit_line(x, y)
-    result = linregress(x / self.train_terms.num_tokens, y_fitted)[0]  # divide x to increase slope
     return result
