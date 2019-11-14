1bc553002315dfdb00b27d43b376a3fd70d8a206
phueb <info@philhuebner.com>
2019-11-14 11:56:46 -0600

    remove redundant scripts to compute correlations

--- scripts/correlation/pos_and_age.py
+++ scripts/correlation/pos_and_age.py
@@ -65,51 +65,7 @@ for age_bin in age_bin2tag_tokens.keys():
     pos_words = probe_store.cat2probes[POS]
     print(len(pos_words))
 
-    # compute num SVO triples as measure of semantic complexity
-    sentences = get_sentences_from_tokens(word_tokens, punctuation={'.', '!', '?'})
-    texts = [' '.join(s) for s in sentences]
-    unique_triples = set()
-    for doc in nlp.pipe(texts):
-        for t in subject_verb_object_triples(doc):  # only returns triples, not partial triples
-            unique_triples.add(t)
-    num_unique_triples_in_part = len(unique_triples)
-    comp = num_unique_triples_in_part
 
-    # co-occurrence matrix
-    tw_mat_observed, xws_observed, _ = make_context_by_term_matrix(word_tokens,
-                                                                   context_size=CONTEXT_SIZE,
-                                                                   shuffle_tokens=False)
-    tw_mat_chance, xws_chance, _ = make_context_by_term_matrix(word_tokens,
-                                                               context_size=CONTEXT_SIZE,
-                                                               shuffle_tokens=True)
+    # TODO implement this script - it should do what pos_and_partition.py does, but instead operates over
+    # TODO age bins rather than equal sized partitions
 
-    # calc selectivity of noun contexts
-    cttr_chance, cttr_observed, sel = calc_selectivity(tw_mat_chance,
-                                                       tw_mat_observed,
-                                                       xws_chance,
-                                                       xws_observed,
-                                                       pos_words)
-    print(f'age_bin={age_bin} selectivity={sel} semantic-complexity={comp}')
-    print()
-
-    # collect
-    x.append(comp)
-    y.append(sel)
-    age_bins.append(age_bin)
-
-# figure
-fig, ax = plt.subplots(1, figsize=(7, 7), dpi=192)
-ax.set_xlabel('Semantic Complexity', fontsize=14)
-ax.set_ylabel(f'{POS}-Context Selectivity\n(context-size={CONTEXT_SIZE})', fontsize=14)
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-ax.tick_params(axis='both', which='both', top=False, right=False)
-# plot
-ax.scatter(x, y, color='black')
-plot_best_fit_line(ax, x, y, fontsize=12, x_pos=0.75, y_pos=0.75)
-for xi, yi, age_bin in zip(x, y, age_bins):
-    s = f'days=\n{age_bin}-{age_bin + AGE_STEP}'
-    ax.text(xi + 10, yi, s, fontsize=10)
-
-plt.tight_layout()
-plt.show()
