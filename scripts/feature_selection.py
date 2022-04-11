fa3911a24bb066973a424b9076ed0dd80f203c42
phueb <info@philhuebner.com>
2022-04-10 19:14:33 -0500

    use chi2 to do feature selection

--- scripts/feature_selection.py
+++ scripts/feature_selection.py
@@ -9,11 +9,13 @@ from collections import Counter
 import numpy as np
 from spacy.tokens.doc import Doc
 from spacy.tokens.token import Token
-from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import mutual_info_classif, chi2
 
 from aochildesnouns.targets import make_targets
 from aochildesnouns.pre_processing import prepare_data
 from aochildesnouns.params import Conditions
+
+USE_FREQ = True  # use counts and chi-square, else use proportion and mutual-info
 
 
 def get_target_contexts(targets: SortedSet,
@@ -43,7 +45,7 @@ def get_target_contexts(targets: SortedSet,
         elif direction == 'r':
             context = tuple([t.tag_ for t in doc[n + 1:n + 1 + context_size]])
         else:
-            raise AttributeError('Invalid arg to direction')
+            raise AttributeError(f'Invalid arg to direction ({direction})')
 
         if not context:
             continue
@@ -81,6 +83,7 @@ def get_target_contexts(targets: SortedSet,
 
 def make_target_vectors(target2contexts: Dict[str, Tuple[str]],
                         contexts_shared: SortedSet,
+                        max_sum: int,  # maximum number of co-occurrence observations
                         ) -> np.ndarray:
     """
     make target representations based on each target's contexts.
@@ -93,10 +96,12 @@ def make_target_vectors(target2contexts: Dict[str, Tuple[str]],
     num_targets = len(target2contexts)
     context2col_id = {c: n for n, c in enumerate(contexts_shared)}
 
-    res = np.zeros((num_targets, num_context_types), int)
+    res = np.zeros((num_targets, num_context_types), float) + 0.0001
+    collected_f = 0
+    total_f = 0
     for row_id, (t, target_contexts) in enumerate(target2contexts.items()):
 
-        assert target_contexts
+        total_f += len(target_contexts)
 
         # make target representation
         c2f = Counter(target_contexts)
@@ -106,7 +111,13 @@ def make_target_vectors(target2contexts: Dict[str, Tuple[str]],
             except KeyError:  # context is not in contexts_all (includes intersection of ctl and exp contexts)
                 pass
             else:
-                res[row_id, col_id] = int(f)
+                res[row_id, col_id] = f
+                collected_f += f
+
+        if collected_f > max_sum:
+            break
+
+    print(f'Collected {collected_f:,}/{total_f:,} co-occurrences')
 
     # check each representation has information
     num_zero_rows = np.sum(~res.any(axis=1))
@@ -115,13 +126,98 @@ def make_target_vectors(target2contexts: Dict[str, Tuple[str]],
     # this assertion fails when only those context types are included that are shared by exp and ctl targets.
     # some targets only occur with those infrequent, non-shared contexts, and so do not have any counts in the matrix
 
+    # row-wise normalization
+    if not USE_FREQ:
+        res = res / res.sum(1)[:, np.newaxis]
+
     return res
+
+
+def feature_selection(params,
+                      targets_exp: SortedSet,
+                      targets_ctl: SortedSet,
+                      doc: Doc,
+                      max_sum: int,
+                      ):
+    """
+    collect contexts, make features, and compute mutual information between contexts and category (exp vs. ctl)
+
+    features are the proportion of times a target co-occurs with a given POS sequence
+    """
+
+    # get experimental target contexts
+    target_exp2contexts, context_exp2f = get_target_contexts(targets_exp,
+                                                             doc,
+                                                             direction=params.direction,
+                                                             context_size=2,
+                                                             preserve_order=True,
+                                                             )
+    # get control target contexts
+    target_ctl2contexts, context_ctl2f = get_target_contexts(targets_ctl,
+                                                             doc,
+                                                             direction=params.direction,
+                                                             context_size=2,
+                                                             preserve_order=True,
+                                                             )
+    # make sure that the same contexts are in both feature matrices (an in the same order)
+    contexts_shared = SortedSet(context_exp2f.copy().keys())
+    contexts_shared.intersection_update(context_ctl2f.keys())
+
+    # make features
+    target_exp_rep_mat = make_target_vectors(target_exp2contexts, contexts_shared, max_sum)
+    target_ctl_rep_mat = make_target_vectors(target_ctl2contexts, contexts_shared, max_sum)
+    print('shape of exp target features={}'.format(target_exp_rep_mat.shape))
+    print('shape of ctl target features={}'.format(target_ctl_rep_mat.shape))
+
+    # feature selection: which contexts are most diagnostic of category membership?
+    X = np.vstack((target_exp_rep_mat, target_ctl_rep_mat))
+    y = np.hstack((
+        np.ones(len(target_exp_rep_mat), dtype=np.int) * 1,
+        np.ones(len(target_ctl_rep_mat), dtype=np.int) * 0
+    ))
+
+    if USE_FREQ:
+        res, p_values = chi2(X, y)  # also returns p values
+        method = 'chi2'
+    else:
+        res = mutual_info_classif(X, y, discrete_features=False)  # mutual info for each column in X
+        p_values = [np.nan] * len(res)
+        method = 'mutual-info'
+
+    # print most diagnostic contexts
+    print('----------------|most diagnostic|------------------------------------------------')
+    for i in np.argsort(res)[::-1][:10]:
+        c = contexts_shared[i]
+        print(f'context={" ".join(c):<32} '
+              f'{method}={res[i]:6.6f} '
+              f'p-val={p_values[i]:6.6f} '
+              f'f-exp={context_exp2f[c]:>6,} '
+              f'f-ctl={context_ctl2f[c]:>6,} ')
+    print('----------------|least diagnostic|-----------------------------------------------')
+    for i in np.argsort(res)[::-1][-10:]:
+        c = contexts_shared[i]
+        print(f'context={" ".join(c):<32} '
+              f'{method}={res[i]:6.6f} '
+              f'p-val={p_values[i]:6.6f} '
+              f'f-exp={context_exp2f[c]:>6,} '
+              f'f-ctl={context_ctl2f[c]:>6,} ')
 
 
 for params in Conditions.all():
 
     age2doc = prepare_data(params)  # returns spacy Doc objects
     targets_exp, targets_ctl = make_targets(params, age2doc)
+
+    params = attr.evolve(params,
+                         age='combined',
+                         targets_control=True,
+                         direction='l'
+                         )
+    print(params)
+
+    # for all ages
+    doc_combined = Doc.from_docs([d for d in age2doc.values()])
+    feature_selection(params, targets_exp, targets_ctl, doc_combined, max_sum=500_000)
 
     # for each age
     for age, doc in sorted(age2doc.items(), key=lambda i: i[0]):
@@ -133,43 +229,4 @@ for params in Conditions.all():
                              )
         print(params)
 
-        # get experimental target representations
-        target_exp2contexts, context_exp2f = get_target_contexts(targets_exp,
-                                                                 doc,
-                                                                 direction=params.direction,
-                                                                 context_size=2,
-                                                                 preserve_order=True,
-                                                                 )
-
-        # get control target representations
-        target_ctl2contexts, context_ctl2f = get_target_contexts(targets_ctl,
-                                                                 doc,
-                                                                 direction=params.direction,
-                                                                 context_size=2,
-                                                                 preserve_order=True,
-                                                                 )
-        # make sure that the same contexts are in both representation matrices (an in the same order)
-        contexts_shared = SortedSet(context_exp2f.copy().keys())
-        contexts_shared.intersection_update(context_ctl2f.keys())
-
-        # make representations
-        target_exp_rep_mat = make_target_vectors(target_exp2contexts, contexts_shared)
-        target_ctl_rep_mat = make_target_vectors(target_ctl2contexts, contexts_shared)
-        print('shape of exp target representations={}'.format(target_exp_rep_mat.shape))
-        print('shape of ctl target representations={}'.format(target_ctl_rep_mat.shape))
-
-        # feature selection: which contexts are most diagnostic of category membership?
-        X = np.vstack((target_exp_rep_mat, target_ctl_rep_mat))
-        y = np.hstack((
-            np.ones(len(target_exp_rep_mat), dtype=np.int) * 1,
-            np.ones(len(target_ctl_rep_mat), dtype=np.int) * 0
-        ))
-        mutual_info = mutual_info_classif(X, y, discrete_features=True)  # mutual info for each column in X
-
-        # print most diagnostic contexts
-        for i in np.argsort(mutual_info)[::-1][:10]:
-            c = contexts_shared[i]
-            print(f'context={" ".join(c):<32} '
-                  f'mutual-info={mutual_info[i]:.6f} '
-                  f'f-exp={context_exp2f[c]:>6,} '
-                  f'f-ctl={context_ctl2f[c]:>6,} ')
+        feature_selection(params, targets_exp, targets_ctl, doc, max_sum=81_000)
